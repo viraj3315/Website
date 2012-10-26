a7bb27f6be96759a88b11ea9be06a4ac06757ef9
Andrew Stiles <astiles@bumptechnologies.com>
2012-10-26 13:02:22 -0700

    added the user specific file

---
+++ bases/user_specific.py
@@ -0,0 +1,3 @@
+DB_NAME = '/Users/aostiles/Dropbox/web_development/django/bases/bases_db'
+TEMPLATE_DIR_LOCATION = '/Users/aostiles/Dropbox/web_development/django/bases/templates'
+SECRET_KEY = '0lvo9tr(l!hb-v2uyg*!_nni9ny4ip-vy*5(fd2w$gk5%!j77z'
